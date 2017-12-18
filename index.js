2ea81e8157b9999311a6a38502b771991e38c873
hehrman@me.com Ehrman <hehrman@me.com>
2017-12-18 20:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays() {
   return "Happy Holidays!";
 }
-function happyHolidaysTo() {
-  return "`Happy holidays, ${name}!`
-  "
+function happyHolidaysTo(name) {
+  return `Happy holidays ${name}!`
+  
