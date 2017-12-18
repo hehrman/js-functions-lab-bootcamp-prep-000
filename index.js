bccc3d429a63ae250f6bfde3344565576fdd8ed0
hehrman@me.com Ehrman <hehrman@me.com>
2017-12-18 20:25:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, day) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
